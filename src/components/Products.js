f8b3896a222b66c88ddabb3b60670d81d265fbcb
rashid-rashie-au7 <you@example.com>
2020-10-16 19:57:11 +0530

    cart-component

--- src/components/Products.js
+++ src/components/Products.js
@@ -16,9 +16,9 @@ export default class Products extends Component {
                                </a>
                                <div className="product-price">
                                    <div>₹ {(product.price).toFixed(2)}</div>
-                                   <button className ="button primary">Add to Cart</button>
+                                   <button onClick={(e) => this.props.addToCart(product)} className ="button primary">Add to Cart</button>
                                </div>
-                           </div>
+                           </div>  
                        </li>
                     ))}
                 </ul>
